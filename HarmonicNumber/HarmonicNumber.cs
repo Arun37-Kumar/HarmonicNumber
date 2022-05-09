77efe546c67a83807d1012c428339ccd627010c4
Arun37-Kumar <arun007624@gmail.com>
2022-05-09 08:48:22 +0530

    Done

---
+++ HarmonicNumber/HarmonicNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber
+{
+    class HarmonicNumber
+    {
+        public void PrintHarmonicNumber(int number)
+        {
+            for(int index = 1; index <= number; index++)
+            {
+                Console.Write("1/"+ index + " + " );
+            }
+        }
+    }
+}
---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            HarmonicNumber harmonic = new HarmonicNumber();
+            harmonic.PrintHarmonicNumber(number);
+            Console.ReadLine();
+
+        }
+
+    }
+}
