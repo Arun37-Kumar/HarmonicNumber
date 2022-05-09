7486b1c6425411d1171ece335b0155ed25b52dfd
Arun37-Kumar <arun007624@gmail.com>
2022-05-09 09:51:56 +0530

    final commit

--- HarmonicNumber/HarmonicNumber.cs
+++ HarmonicNumber/HarmonicNumber.cs
@@ -11,12 +11,11 @@ namespace HarmonicNumber
         public void PrintHarmonicNumber(int number)
         {
             float sumOfHarmonicNumbers = 0;
-            for (int index = 1; index <= number; index++)
+            for (float index = 1; index <= number; index++)
             {
                 sumOfHarmonicNumbers = (sumOfHarmonicNumbers) + (float)(1 / index);
             }
             Console.WriteLine("Sum is : "+ sumOfHarmonicNumbers);
-            //Console.Write("1/" + index + " + ");
         }
     }
 }
